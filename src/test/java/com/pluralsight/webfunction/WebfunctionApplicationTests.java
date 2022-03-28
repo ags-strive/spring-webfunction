4be8b3faa006a719e673ef3ba75836dc3cc81f8c
ags-strive <aspencer@striveconsulting.com>
2022-03-28 12:17:17 -0500

    init commit

---
+++ src/test/java/com/pluralsight/webfunction/WebfunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.webfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
